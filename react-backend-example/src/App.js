2905ba68b8b10bf2a903585992479c498718cdb1
Knut Martin Tornes <knutmt@gmail.com>
2023-01-11 20:28:33 +0100

    not old school react

--- react-backend-example/src/App.js
+++ react-backend-example/src/App.js
@@ -1,49 +1,45 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import React from 'react';
 import './App.css';
 
 // React backend API endpoint and API token
 const API_ENDPOINT = 'https://reactback-nyly.api.codehooks.io/dev/hello';
 const API_KEY = 'a4679c85-b4c8-49fb-b8ac-63230b269dd7';
 
-class App extends React.Component {
-  // Application state
-  constructor() {
-    super();
-    this.state = { message: "loading ...", visits: undefined };
-  }
+function App() {
 
-  async componentDidMount() {
-    // call backend api at start
-    const response = await fetch(API_ENDPOINT, {
-      method: "GET",
-      headers: { "x-apikey": API_KEY }
-    })
-    // get message and visits data from json response
-    const {message, visits} = await response.json();
-    // set app state and reload
-    this.setState({ message, visits });
-  }
+  const [visits, setVisits] = useState(null);
+  const [message, setMessage] = useState(null);
 
-  render() {
-    const { message, visits } = this.state;
-    return (
-      <div className="App">
+  useEffect(()=>{
+    const fetchData = async () => {
+      const response = await fetch(API_ENDPOINT, {
+        method: "GET",
+        headers: { "x-apikey": API_KEY }
+      });
+      const data = await response.json();
+      setMessage(data.message);
+      setVisits(data.visits);
+    }
+    fetchData();
+  },[])
+
+  return (
+    <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>
             React backend with Codehooks.io
           </h2>
-          <h2 className="heading">
-            {message}
+          <h2 style={{height: '50px'}} className="heading">
+            {message || ''}
           </h2> 
           <p>
-            Visitors: {visits}
+            Visitors: {visits || '---'}
           </p>          
         </header>
       </div>
-    );
-  }
+  );
 }
 
 export default App;
