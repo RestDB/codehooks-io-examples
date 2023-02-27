8d13148b1d252a33abb4550f12464adb07dae66d
Jon Erik Solheim <jones@restdb.io>
2023-02-27 17:59:08 +0100

    minimal app

--- codehooks-mongodb/myapp/index.js
+++ codehooks-mongodb/myapp/index.js
@@ -1,19 +1,9 @@
 /* index.js
 *  Example codehooks-mongodb backend app
 */
-import {app, Datastore} from 'codehooks-js'
+import app from 'codehooks-js'
 import crudlify from 'codehooks-crudlify-yup'
 import {user} from './schema.js'; // Yup data schema definition
-
-import message from './sendmessage.js' // Helper function for sending messages
-import queueIt from './queueHelper.js' // Worker function for queued functions
-
-// call the imported local lib function
-app.post('/message', message)
-
-// queue worker function to topic
-app.queue('messageTopic', queueIt)
-
 
 // Add CRUD routes for a user schema - collection
 crudlify(app, {user})
