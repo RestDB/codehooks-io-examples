9162caac9d609d7790c42938a4083514bc4af8ac
Jon Erik Solheim <jones@restdb.io>
2023-03-12 11:19:27 +0100

    Crud blog

--- crud/middleware.js
+++ crud/middleware.js
@@ -1,6 +1,6 @@
 // calculate balance from customer.products array of prices
 export function middleware(req, res, next) {    
-    if (req.method === 'POST' && req.params.collection === 'customer') {
+    if (req.method === 'POST') {
         if (req.body.products) {
             req.body.balance = req.body.products.reduce((accumulator, object) => {
                 return accumulator + object.price;
