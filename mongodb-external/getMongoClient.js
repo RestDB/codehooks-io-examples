a8533bf0f96bfde3deaa96e817b98553dae44cb8
restdbjones <jones@restdb.io>
2023-07-17 09:46:08 +0200

    Refactor

---
+++ mongodb-external/getMongoClient.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb-3";
+
+export const connectionString = process.env.MONGO_URI;
+
+export let mongoConn = null;
+
+export function getMongoClient() {
+    return new Promise((resolve, reject) => {
+        if (mongoConn) {
+            // cached conn
+            return resolve(mongoConn);
+        }
+        console.log('conn str', process.env);
+        MongoClient.connect(connectionString, function (err, conn) {
+            if (err || !conn) {
+                console.log("Err connect", err, conn);
+                return reject(err);
+            }
+            mongoConn = conn.db("userdb");
+
+            console.log("Successfully connected to MongoDB.");
+            resolve(mongoConn);
+        });
+    });
+}
--- mongodb-external/index.js
+++ mongodb-external/index.js
@@ -3,31 +3,7 @@
 * Codehooks (c) mongoDB example
 */
 import { app } from 'codehooks-js'
-import { MongoClient } from "mongodb-3";
-
-const connectionString = process.env.MONGO_URI;
-
-let mongoConn = null;
-
-function getMongoClient() {
-    return new Promise((resolve, reject) => {
-        if (mongoConn) {
-            // cached conn
-            return resolve(mongoConn)
-        }
-        console.log('conn str', process.env)
-        MongoClient.connect(connectionString, function (err, conn) {
-            if (err || !conn) {
-                console.log("Err connect", err, conn)
-                return reject(err);
-            }
-            mongoConn = conn.db("userdb");
-
-            console.log("Successfully connected to MongoDB.");
-            resolve(mongoConn);
-        });
-    })
-}
+import { getMongoClient } from './getMongoClient';
 
 app.get('/getmongodbdata', async (req, res) => {
     try {
