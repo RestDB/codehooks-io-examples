33bf7a3fd8ac7f41f15ba2f422aa98b14d54876a
Knut Martin Torn knutmt@gmail.com <knutmt@gmail.com>
2024-11-27 19:13:58 +0100

    updated realtime example

--- realtime-chat/index.js
+++ realtime-chat/index.js
@@ -3,33 +3,33 @@
 * Codehooks.io - Chat example
 */
 
-import {app, realtime} from 'codehooks-js';
+import { app, realtime } from 'codehooks-js';
 
 // create a real-time channel
 realtime.createChannel('/chat');
+
+
+app.auth("/*", (req, res, next) => {
+  console.log('No auth today', req.path)
+  next()
+});
 
 // client connects for a new real-time listener ID
 app.post('/connect', async (req, res) => {
   const listenerData = await realtime.createListener('/chat')
   // return listener ID to client
-  res.json({listenerID: listenerData._id})
+  res.json({ listenerID: listenerData._id })
 })
 
 // client post a new message
 app.post('/messages', async (req, res) => {
   console.log('Message in', req.body)
-  const {message, listenerID, alias} = req.body;
-  const data = await realtime.publishEvent('/chat', {"message": `${alias || 'Anonymous'}: ${message}`});
+  const { message, listenerID, alias } = req.body;
+  const data = await realtime.publishEvent('/chat', { "message": `${alias || 'Anonymous'}: ${message}` });
   res.end(data)
 })
 
-app.job('*/2 * * * *', async (_, job) => {
-  const message = `Hello from cron job at ${new Date().toISOString()}`;
-  const data = await realtime.publishEvent('/chat', {"message": `cron: ${message}`});
-  job.end()
-})
-
-app.static({route:'/public', directory: '/public'})
+app.static({ route: '/public', directory: '/public' })
 
 // bind to serverless runtime
 export default app.init();
